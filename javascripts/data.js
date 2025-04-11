ba2b1d3c4b67a775a7c3f711f4108f4d17fa8c62
jennyhwaang <yongbabyactress@gmail.com>
2025-04-11 16:41:48 -0700

    created coffee catalog array
    added vanilla latte, hazelnut latte, mocha latte

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,11 +1,24 @@
 let coffeearray=[
     {
-        type:"vanilla latte",
+        name:"Vanilla latte",
         price:6.99,
+        type:"Iced or Hot",
+        milk:"Oat milk, Almond, Dairy, or Soy",
+        caffeine:"90",
+        rating:"4.7",
+        description:"Creamy coffee made with espresso, steamed milk, and sweet vanilla syrup",
+        ingredients:["espresso","steamed milk", "vanilla syrup"],
+        image:"assets/vanillalatte.webp",
     },
     {
-        type:"hazelnut latte",
+        name:"Hazelnut latte",
         price:6.49,
-
+        type:"Hot or Iced",
+        milk:"Oat milk, Almond, Dairy, or Soy",
+        caffeine:"90",
+        rating:"4.6",
+        description:"Espresso with steamed milk and rich hazelnut flavor",
+        ingredients:["espresso","steamed milk","hazelnut syrup"],
+        image:"assets/"
     }
 ];
